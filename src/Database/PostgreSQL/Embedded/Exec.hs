9f6f0478ef8211111febd06b40a9cb6146c1c397
Ilya Murzinov <murz42@gmail.com>
2018-02-18 21:50:31 +0300

    Fix GHC 7.8.4 build

--- src/Database/PostgreSQL/Embedded/Exec.hs
+++ src/Database/PostgreSQL/Embedded/Exec.hs
@@ -13,10 +13,15 @@ import           System.Process
 Exactly like 'System.Process.system' but does not capture stdout nor stderr.
 -}
 systemSilent :: String -> IO ExitCode
-systemSilent "" = ioException (ioeSetErrorString (mkIOError InvalidArgument "system" Nothing Nothing) "null command")
+systemSilent "" = ioException
+  (ioeSetErrorString (mkIOError InvalidArgument "system" Nothing Nothing) "null command")
 systemSilent str = do
   devNull <- openFile "/dev/null" WriteMode
-  (_, _, _, p) <- createProcess_ "system" (shell str) {delegate_ctlc = True, std_err = UseHandle devNull, std_out = UseHandle devNull}
+  (_, _, _, p) <- createProcess (shell str)
+                  { delegate_ctlc = True
+                  , std_err = UseHandle devNull
+                  , std_out = UseHandle devNull
+                  }
   waitForProcess p
 
 {-|
@@ -25,13 +30,17 @@ Exactly like 'System.Process.rawSystem' but does not capture stdout nor stderr.
 rawSystemSilent :: String -> [String] -> IO ExitCode
 rawSystemSilent cmd args = do
   devNull <- nullDevice
-  (_, _, _, p) <- createProcess_ "rawSystem" (proc cmd args) {delegate_ctlc = True, std_err = UseHandle devNull, std_out = UseHandle devNull}
+  (_, _, _, p) <- createProcess (proc cmd args)
+                  { delegate_ctlc = True
+                  , std_err = UseHandle devNull
+                  , std_out = UseHandle devNull
+                  }
   waitForProcess p
 
 nullDevice :: IO Handle
 nullDevice = do
   h <- tryIOError nix
-  either (const windows) pure h
+  either (const windows) return h
   where
     nix = openFile "/dev/null" WriteMode
     windows = openFile "nul" WriteMode
